5b17f059c342cc573420bb0ece4a81d29ae99901
Bhavya-rathod <rathodbnv@gmail.com>
2025-12-17 11:43:58 +0530

    Initial commit

---
+++ assignment 1 task 1 .py.py
@@ -0,0 +1,13 @@
+# Taking two numbers as input form user
+num1=float(input("Enter a number: "))
+num2=float(input("Enter another number: "))
+# The basic mathematical operations
+addition=int(num1+num2)
+subtraction=int(num1-num2)
+multiplication=int(num1*num2)
+division=(num1/num2)
+# Displaying the results
+print("Addition :", addition)
+print("Subtraction :", subtraction)
+print("Multiplication :", multiplication)
+print("Division :", division)
---
+++ assignment 1 task 2  .py.py
@@ -0,0 +1,11 @@
+# Take user input
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate first and last name
+full_name = first_name + " " + last_name
+
+# Print a personalized greeting
+print("Hello, " + full_name + "! Welcome to the python program !")
+
+
